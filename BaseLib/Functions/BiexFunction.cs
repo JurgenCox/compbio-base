5b7245f798b34c45645241f30c39aeb57fb4ac75
Daniela Ferretti <ferretti.daniela@outlook.com>
2019-05-17 16:10:27 +0200

    CyTOF: Biexponential prep

---
+++ BaseLib/Functions/BiexFunction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseLib.Functions
+{
+    public class BiexFunction
+    {
+        public double TopScale(IList<double> values)
+        {
+            double[] result = new double[values.Count];
+            double largest = result[0];
+            for (int i = 1; i < result.Length; i++)
+            {
+                if (result[i] > largest)
+                    largest = result[i];
+            }
+            return largest;
+        }
+
+
+        public double MinScale(IList<double> values)
+        {
+            double[] result = new double[values.Count];
+            double min = result[0];
+            for (int i = 1; i < result.Length; i++)
+            {
+                if (result[i] < min)
+                    min = result[i];
+            }
+            return min;
+        }
+
+    }
+}
---
+++ BaseLib/Functions/BiexTransform.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using BaseLib.Forms.Base;
+using BaseLibS.Graph;
+using BaseLibS.Graph.Axis;
+using BaseLib.Forms;
+
+namespace BaseLib.Functions
+{
+    public partial class BiexTransform : Form
+    {
+
+        public BiexTransform()
+        {
+            InitializeComponent();
+        }
+
+    
+
+        public string TextBoxMinValue { get { return minvalue.Text; } set { minvalue.Text = value; } }
+        public string TextBoxMaxValue { get { return maxvalue.Text; } set { maxvalue.Text = value; } }
+
+        public string MinValue {  get { return minvalue.Text; } set { minvalue.Text = value; } }
+        public string MaxValue {  get { return maxvalue.Text; } set { maxvalue.Text = value; } }
+
+        public int NegTrackMinValue {  get { return NegValues.Minimum; } set { NegValues.Minimum = value; } }
+        public int BasisTrackMinValue { get { return trackbasis.Minimum; } set { trackbasis.Minimum = value; } }
+        public int NegTrackMaxValue { get { return NegValues.Maximum; } set { NegValues.Maximum = value; } }
+        public int BasisTrackMaxValue { get { return trackbasis.Maximum; } set { trackbasis.Maximum = value; } }
+
+
+        private void BiexTransform_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void basisvalue_Click(object sender, EventArgs e)
+        {
+            basisvalue.Text = trackbasis.Value.ToString();
+        }
+
+        private void trackbasis_Scroll(object sender, EventArgs e)
+        {
+            basisvalue.Text = trackbasis.Value.ToString();
+        }
+
+        private void NegValues_Scroll(object sender, EventArgs e)
+        {
+            n_value.Text = NegValues.Value.ToString();
+        }
+
+        private void n_value_Click(object sender, EventArgs e)
+        {
+            n_value.Text = NegValues.Value.ToString();
+        }
+    }
+}
