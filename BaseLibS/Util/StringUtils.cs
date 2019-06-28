30691a0f554307a5f5101d2818361ccaf519fe5e
Juergen Cox <jurgen.cox@hotmail.com>
2019-06-28 10:45:29 +0200

    minor

--- BaseLibS/Util/StringUtils.cs
+++ BaseLibS/Util/StringUtils.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
-using BaseLibS.Num.Test;
 
 namespace BaseLibS.Util {
 	public static class StringUtils {
