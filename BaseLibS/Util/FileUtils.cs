a196dfe560c93eb70b8c21c5cf9448580b970f43
Juergen Cox <cox@biochem.mpg.de>
2019-11-13 16:14:46 +0100

    more methods in FileUtils

--- BaseLibS/Util/FileUtils.cs
+++ BaseLibS/Util/FileUtils.cs
@@ -307,6 +307,17 @@ namespace BaseLibS.Util{
 			return result;
 		}
 
+		public static Dictionary<int, double> ReadDictionaryIntDouble(BinaryReader reader){
+			int len = reader.ReadInt32();
+			Dictionary<int, double> result = new Dictionary<int, double>();
+			for (int i = 0; i < len; i++){
+				int key = reader.ReadInt32();
+				double value = reader.ReadDouble();
+				result.Add(key, value);
+			}
+			return result;
+		}
+
 		/// <summary>
 		/// Removes all files and folders recursively in the specified folder 
 		/// and the specified folder itself.
@@ -778,6 +789,14 @@ namespace BaseLibS.Util{
 		}
 
 		public static void Write(IDictionary<int, int> x, BinaryWriter writer){
+			writer.Write(x.Count);
+			foreach (int key in x.Keys){
+				writer.Write(key);
+				writer.Write(x[key]);
+			}
+		}
+
+		public static void Write(IDictionary<int, double> x, BinaryWriter writer){
 			writer.Write(x.Count);
 			foreach (int key in x.Keys){
 				writer.Write(key);
