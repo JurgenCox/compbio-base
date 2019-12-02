6171f1bc91fdb8bce2311a8628fbb9c0e162aa8e
Juergen Cox <cox@biochem.mpg.de>
2019-12-02 14:15:17 +0100

    Saving memory for constant matrices

--- BaseLibS/Num/Matrix/BoolMatrixIndexer.cs
+++ BaseLibS/Num/Matrix/BoolMatrixIndexer.cs
@@ -1,107 +1,153 @@
 ﻿using System;
 
-namespace BaseLibS.Num.Matrix {
+namespace BaseLibS.Num.Matrix{
 	[Serializable]
-	public class BoolMatrixIndexer : IBoolMatrixIndexer {
+	public class BoolMatrixIndexer : IBoolMatrixIndexer{
 		private bool[,] vals;
+		private bool isConstant;
+		private readonly bool constVal;
+		private int nrows;
+		private int ncols;
 
-		public BoolMatrixIndexer(bool[,] vals) {
+		public BoolMatrixIndexer(bool[,] vals){
 			this.vals = vals;
 		}
 
-		public BoolMatrixIndexer() { }
+		public BoolMatrixIndexer(bool val, int nrows, int ncols){
+			isConstant = true;
+			constVal = val;
+			this.nrows = nrows;
+			this.ncols = ncols;
+		}
 
-		public void Init(int nrows, int ncols) {
+		public BoolMatrixIndexer(){ }
+
+		public void Init(int nrows, int ncols){
+			isConstant = false;
 			vals = new bool[nrows, ncols];
 		}
 
-
-
-        public IBoolMatrixIndexer Transpose() {
+		public IBoolMatrixIndexer Transpose(){
+			if (isConstant){
+				return new BoolMatrixIndexer(constVal, ncols, nrows);
+			}
 			return vals == null ? new BoolMatrixIndexer() : new BoolMatrixIndexer(ArrayUtils.Transpose(vals));
 		}
 
-		public void TransposeInPlace() {
+		public void TransposeInPlace(){
+			if (isConstant){
+				int tmp = nrows;
+				nrows = ncols;
+				ncols = tmp;
+				return;
+			}
 			vals = ArrayUtils.Transpose(vals);
 		}
 
-		public void Set(bool[,] value) {
+		public void Set(bool[,] value){
+			isConstant = false;
 			vals = value;
 		}
 
-		public bool[,] Get() {
+		public bool[,] Get(){
 			return vals;
 		}
 
-		public bool[] GetRow(int row) {
+		public bool[] GetRow(int row){
 			bool[] result = new bool[ColumnCount];
-			for (int i = 0; i < result.Length; i++) {
-				result[i] = vals[row, i];
+			for (int i = 0; i < result.Length; i++){
+				result[i] = isConstant ? constVal : vals[row, i];
 			}
 			return result;
 		}
 
-		public bool[] GetColumn(int col) {
+		public bool[] GetColumn(int col){
 			bool[] result = new bool[RowCount];
-			for (int i = 0; i < result.Length; i++) {
-				result[i] = vals[i, col];
+			for (int i = 0; i < result.Length; i++){
+				result[i] = isConstant ? constVal : vals[i, col];
 			}
 			return result;
 		}
 
-		public bool IsInitialized() {
-			return vals != null;
+		public bool IsInitialized(){
+			return vals != null || isConstant;
 		}
 
-		public void ExtractRowsInPlace(int[] rows) {
-			if (vals != null) {
+		public void ExtractRowsInPlace(int[] rows){
+			if (isConstant){
+				nrows = rows.Length;
+				return;
+			}
+			if (vals != null){
 				vals = ArrayUtils.ExtractRows(vals, rows);
 			}
 		}
 
-		public void ExtractColumnsInPlace(int[] columns) {
-			if (vals != null) {
+		public void ExtractColumnsInPlace(int[] columns){
+			if (isConstant){
+				ncols = columns.Length;
+				return;
+			}
+			if (vals != null){
 				vals = ArrayUtils.ExtractColumns(vals, columns);
 			}
 		}
 
-		public IBoolMatrixIndexer ExtractRows(int[] rows) {
+		public IBoolMatrixIndexer ExtractRows(int[] rows){
+			if (isConstant){
+				return new BoolMatrixIndexer(constVal, rows.Length, ncols);
+			}
 			return new BoolMatrixIndexer(ArrayUtils.ExtractRows(vals, rows));
 		}
 
-		public IBoolMatrixIndexer ExtractColumns(int[] columns) {
+		public IBoolMatrixIndexer ExtractColumns(int[] columns){
+			if (isConstant){
+				return new BoolMatrixIndexer(constVal, nrows, columns.Length);
+			}
 			return new BoolMatrixIndexer(ArrayUtils.ExtractColumns(vals, columns));
 		}
 
-		public int RowCount => vals.GetLength(0);
-		public int ColumnCount => vals.GetLength(1);
+		public int RowCount => isConstant ? nrows : vals?.GetLength(0) ?? 0;
+		public int ColumnCount => isConstant ? ncols : vals?.GetLength(1) ?? 0;
 
-		public bool this[int i, int j] {
-			get => vals[i, j];
+		public bool this[int i, int j]{
+			get => isConstant ? constVal : vals[i, j];
 			set => vals[i, j] = value;
 		}
 
-		public void Dispose() {
+		public void Dispose(){
 			vals = null;
 		}
 
-		public object Clone() {
+		public object Clone(){
+			if (isConstant){
+				return new BoolMatrixIndexer(constVal, nrows, ncols);
+			}
 			return vals == null ? new BoolMatrixIndexer(null) : new BoolMatrixIndexer((bool[,]) vals.Clone());
 		}
 
-		public bool Equals(IBoolMatrixIndexer other) {
-			if (other == null) {
+		public bool Equals(IBoolMatrixIndexer other){
+			if (other == null){
 				return false;
 			}
-			if (!IsInitialized() && !other.IsInitialized()) {
+			if (!IsInitialized() && !other.IsInitialized()){
 				return true;
 			}
-			if (!other.IsInitialized()) {
+			if (!other.IsInitialized()){
 				return false;
 			}
-			for (int i = 0; i < RowCount; i++) {
-				for (int j = 0; j < ColumnCount; j++) {
-					if (this[i, j] != other[i, j]) {
+			if (isConstant){
+				if (nrows != other.RowCount || ncols != other.ColumnCount){
+					return false;
+				}
+				if (nrows == 0 || ncols == 0){
+					return true;
+				}
+				return this[0, 0] == other[0, 0];
+			}
+			for (int i = 0; i < RowCount; i++){
+				for (int j = 0; j < ColumnCount; j++){
+					if (this[i, j] != other[i, j]){
 						return false;
 					}
 				}
--- BaseLibS/Num/Matrix/FloatMatrixIndexer.cs
+++ BaseLibS/Num/Matrix/FloatMatrixIndexer.cs
@@ -2,83 +2,122 @@
 using System.Collections.Generic;
 using BaseLibS.Num.Vector;
 
-namespace BaseLibS.Num.Matrix {
+namespace BaseLibS.Num.Matrix{
 	[Serializable]
-	public class FloatMatrixIndexer : MatrixIndexer {
+	public class FloatMatrixIndexer : MatrixIndexer{
 		private float[,] vals;
-		public FloatMatrixIndexer() { }
+		private bool isConstant;
+		private readonly float constVal;
+		private int nrows;
+		private int ncols;
+		public FloatMatrixIndexer(){ }
 
-		public FloatMatrixIndexer(float[,] vals) {
+		public FloatMatrixIndexer(float[,] vals){
 			this.vals = vals;
 		}
 
-		public override void Init(int nrows, int ncols) {
-			vals = new float[nrows, ncols];
+		public FloatMatrixIndexer(float val, int nrows, int ncols){
+			isConstant = true;
+			constVal = val;
+			this.nrows = nrows;
+			this.ncols = ncols;
 		}
 
-		public void TransposeInPlace() {
-			if (vals != null) {
+		public override void Init(int nrows, int ncols){
+			vals = new float[nrows, ncols];
+			isConstant = false;
+		}
+
+		public void TransposeInPlace(){
+			if (isConstant){
+				int tmp = nrows;
+				nrows = ncols;
+				ncols = tmp;
+				return;
+			}
+			if (vals != null){
 				vals = ArrayUtils.Transpose(vals);
 			}
 		}
 
-		public override MatrixIndexer Transpose() {
+		public override MatrixIndexer Transpose(){
+			if (isConstant){
+				return new FloatMatrixIndexer(constVal, ncols, nrows);
+			}
 			return vals == null ? new FloatMatrixIndexer() : new FloatMatrixIndexer(ArrayUtils.Transpose(vals));
 		}
 
-		public override void Set(double[,] value) {
+		public override void Set(double[,] value){
+			isConstant = false;
 			vals = new float[value.GetLength(0), value.GetLength(1)];
-			for (int i = 0; i < value.GetLength(0); i++) {
-				for (int j = 0; j < value.GetLength(1); j++) {
+			for (int i = 0; i < value.GetLength(0); i++){
+				for (int j = 0; j < value.GetLength(1); j++){
 					vals[i, j] = (float) value[i, j];
 				}
 			}
 		}
 
-		public override BaseVector GetRow(int row) {
+		public override BaseVector GetRow(int row){
 			float[] result = new float[ColumnCount];
-			for (int i = 0; i < result.Length; i++) {
-				result[i] = vals[row, i];
+			for (int i = 0; i < result.Length; i++){
+				result[i] = isConstant ? constVal : vals[row, i];
 			}
 			return new FloatArrayVector(result);
 		}
 
-		public override BaseVector GetColumn(int col) {
+		public override BaseVector GetColumn(int col){
 			float[] result = new float[RowCount];
-			for (int i = 0; i < result.Length; i++) {
-				result[i] = vals[i, col];
+			for (int i = 0; i < result.Length; i++){
+				result[i] = isConstant ? constVal : vals[i, col];
 			}
 			return new FloatArrayVector(result);
 		}
 
-		public override bool IsInitialized() {
-			return vals != null;
+		public override bool IsInitialized(){
+			return vals != null || isConstant;
 		}
 
-		public override MatrixIndexer ExtractRows(IList<int> rows) {
+		public override MatrixIndexer ExtractRows(IList<int> rows){
+			if (isConstant){
+				return new FloatMatrixIndexer(constVal, rows.Count, ncols);
+			}
 			return new FloatMatrixIndexer(ArrayUtils.ExtractRows(vals, rows));
 		}
 
-		public override MatrixIndexer ExtractColumns(IList<int> columns) {
+		public override MatrixIndexer ExtractColumns(IList<int> columns){
+			if (isConstant){
+				return new FloatMatrixIndexer(constVal, nrows, columns.Count);
+			}
 			return new FloatMatrixIndexer(ArrayUtils.ExtractColumns(vals, columns));
 		}
 
-		public override void ExtractRowsInPlace(IList<int> rows) {
-			if (vals != null) {
+		public override void ExtractRowsInPlace(IList<int> rows){
+			if (isConstant){
+				nrows = rows.Count;
+				return;
+			}
+			if (vals != null){
 				vals = ArrayUtils.ExtractRows(vals, rows);
 			}
 		}
 
-		public override void ExtractColumnsInPlace(IList<int> columns) {
-			if (vals != null) {
+		public override void ExtractColumnsInPlace(IList<int> columns){
+			if (isConstant){
+				ncols = columns.Count;
+				return;
+			}
+			if (vals != null){
 				vals = ArrayUtils.ExtractColumns(vals, columns);
 			}
 		}
 
-		public override bool ContainsNaNOrInf() {
-			for (int i = 0; i < vals.GetLength(0); i++) {
-				for (int j = 0; j < vals.GetLength(1); j++) {
-					if (float.IsNaN(vals[i, j]) || float.IsInfinity(vals[i, j])) {
+		public override bool ContainsNaNOrInf(){
+			if (isConstant){
+				return float.IsInfinity(constVal) || float.IsNaN(constVal);
+			}
+			for (int i = 0; i < vals.GetLength(0); i++){
+				for (int j = 0; j < vals.GetLength(1); j++){
+					if (float.IsNaN(vals[i, j]) || float.IsInfinity(vals[i, j])){
 						return true;
 					}
 				}
@@ -86,50 +125,65 @@ namespace BaseLibS.Num.Matrix {
 			return false;
 		}
 
-		public override bool IsNanOrInfRow(int row) {
-			for (int i = 0; i < ColumnCount; i++) {
+		public override bool IsNanOrInfRow(int row){
+			if (isConstant){
+				return float.IsInfinity(constVal) || float.IsNaN(constVal);
+			}
+			for (int i = 0; i < ColumnCount; i++){
 				float v = vals[row, i];
-				if (!float.IsNaN(v) && !float.IsInfinity(v)) {
+				if (!float.IsNaN(v) && !float.IsInfinity(v)){
 					return false;
 				}
 			}
 			return true;
 		}
 
-		public override bool IsNanOrInfColumn(int column) {
-			for (int i = 0; i < RowCount; i++) {
+		public override bool IsNanOrInfColumn(int column){
+			if (isConstant){
+				return float.IsInfinity(constVal) || float.IsNaN(constVal);
+			}
+			for (int i = 0; i < RowCount; i++){
 				float v = vals[i, column];
-				if (!float.IsNaN(v) && !float.IsInfinity(v)) {
+				if (!float.IsNaN(v) && !float.IsInfinity(v)){
 					return false;
 				}
 			}
 			return true;
 		}
 
-		public override int RowCount => vals?.GetLength(0) ?? 0;
-		public override int ColumnCount => vals?.GetLength(1) ?? 0;
+		public override int RowCount => isConstant ? nrows : vals?.GetLength(0) ?? 0;
+		public override int ColumnCount => isConstant ? ncols : vals?.GetLength(1) ?? 0;
 
-		public override double this[int i, int j] {
-			get => vals[i, j];
+		public override double this[int i, int j]{
+			get => isConstant ? constVal : vals[i, j];
 			set => vals[i, j] = (float) value;
 		}
 
-		public override double Get(int i, int j) {
+		public override double Get(int i, int j){
+			if (isConstant){
+				return constVal;
+			}
 			return !IsInitialized() ? float.NaN : vals[i, j];
 		}
 
-		public override void Set(int i, int j, double value) {
-			if (!IsInitialized()) {
+		public override void Set(int i, int j, double value){
+			if (isConstant){
+				throw new Exception("Setting value in constant matrix.");
+			}
+			if (!IsInitialized()){
 				return;
 			}
 			vals[i, j] = (float) value;
 		}
 
-		public override void Dispose() {
+		public override void Dispose(){
 			vals = null;
 		}
 
-		public override object Clone() {
+		public override object Clone(){
+			if (isConstant){
+				return new FloatMatrixIndexer(constVal, nrows, ncols);
+			}
 			return vals == null ? new FloatMatrixIndexer(null) : new FloatMatrixIndexer((float[,]) vals.Clone());
 		}
 	}
