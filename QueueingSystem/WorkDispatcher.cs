167ce053f1fd1e1b5f7435302f8c3f8927a21dbe
Juergen Cox <cox@biochem.mpg.de>
2020-03-21 00:00:22 +0100

    scalable toplevel WorkDispatcher

--- QueueingSystem/WorkDispatcher.cs
+++ QueueingSystem/WorkDispatcher.cs
@@ -4,89 +4,98 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using BaseLibS.Util;
 using QueueingSystem.Drmaa;
 using QueueingSystem.GenericCluster;
 using QueueingSystem.Kubernetes;
 
-namespace QueueingSystem {
-	public abstract class WorkDispatcher {
+namespace QueueingSystem{
+	public abstract class WorkDispatcher{
 		private const int initialDelay = 6;
+		private const string clusterTypeDrmaa = "drmaa";
+		private const string clusterTypeGeneric = "generic";
+		private const string clusterTypeKubernetes = "kubernetes";
 		private Thread[] workThreads;
 		private Process[] externalProcesses;
 		private string[] queuedJobIds;
 		private Stack<int> toBeProcessed;
-		public string InfoFolder { get; }
-		public bool DotNetCore { get; }
+		public string InfoFolder{ get; }
+		public bool DotNetCore{ get; }
 		internal readonly int numInternalThreads;
-		private ISession _session;
+		private readonly ISession session;
+		public CalculationType CalculationType{ get; }
 
-		private const string ClusterTypeDrmaa = "drmaa";
-		private const string ClusterTypeGeneric = "generic";
-		private const string ClusterTypeKubernetes = "kubernetes";
-		
-		protected WorkDispatcher(int nThreads, int nTasks, string infoFolder, CalculationType calculationType,
-			bool dotNetCore) : this(nThreads, nTasks, infoFolder, calculationType, dotNetCore, 1)
-		{
-			
-		}
+		public int MaxHeapSizeGb{ get; set; }
+
+		public int Nthreads{ get; }
+
+		public Func<int> NTasks{ get; }
 
 		protected WorkDispatcher(int nThreads, int nTasks, string infoFolder, CalculationType calculationType,
-			bool dotNetCore, int numInternalThreads) {
-			Nthreads = Math.Min(nThreads, nTasks);
+			bool dotNetCore) : this(nThreads, () => nTasks, infoFolder, calculationType, dotNetCore, 1){ }
+
+		protected WorkDispatcher(int nThreads, Func<int> nTasks, string infoFolder, CalculationType calculationType,
+			bool dotNetCore) : this(nThreads, nTasks, infoFolder, calculationType, dotNetCore, 1){ }
+
+		protected WorkDispatcher(int nThreads, int nTasks, string infoFolder, CalculationType calculationType,
+			bool dotNetCore, int numInternalThreads) : this(nThreads, () => nTasks, infoFolder, calculationType,
+			dotNetCore, numInternalThreads){ }
+
+		protected WorkDispatcher(int nThreads, Func<int> nTasks, string infoFolder, CalculationType calculationType,
+			bool dotNetCore, int numInternalThreads){
+			Nthreads = nThreads;
 			this.numInternalThreads = numInternalThreads;
 			NTasks = nTasks;
-			this.InfoFolder = infoFolder;
-			this.DotNetCore = dotNetCore;
-			if (!string.IsNullOrEmpty(infoFolder) && !Directory.Exists(infoFolder)) {
+			InfoFolder = infoFolder;
+			DotNetCore = dotNetCore;
+			if (!string.IsNullOrEmpty(infoFolder) && !Directory.Exists(infoFolder)){
 				Directory.CreateDirectory(infoFolder);
 			}
 			CalculationType = calculationType;
-			
+
 			// TODO: remove in release
-			if (Environment.GetEnvironmentVariable("MQ_CALC_TYPE") == "queue")
-			{
+			if (Environment.GetEnvironmentVariable("MQ_CALC_TYPE") == "queue"){
 				CalculationType = CalculationType.Queueing;
-				_session = GetSession();
-				Console.WriteLine($"Using queueing session type: {_session}");
+				session = GetSession();
+				Console.WriteLine($"Using queueing session type: {session}");
 			}
-			
 		}
-		private static ISession GetSession()
-		{
-			var type = Environment.GetEnvironmentVariable("MQ_CLUSTER_TYPE") ?? ClusterTypeDrmaa;
-			switch (type)
-			{
-				case ClusterTypeDrmaa:
-				{
+
+		private int nTasksCached = -1;
+		private int NTasksCached{
+			get{
+				if (nTasksCached == -1){
+					nTasksCached = NTasks();
+				}
+				return nTasksCached;
+			}
+		}
+
+		private static ISession GetSession(){
+			var type = Environment.GetEnvironmentVariable("MQ_CLUSTER_TYPE") ?? clusterTypeDrmaa;
+			switch (type){
+				case clusterTypeDrmaa:{
 					var s = DrmaaSession.GetInstance();
 					s.Init();
 					var nativeSpec = Environment.GetEnvironmentVariable("MQ_DRMAA_NATIVE_SPEC");
-					if (nativeSpec != null)
-					{
+					if (nativeSpec != null){
 						s.NativeSpecificationTemplate = nativeSpec;
 					}
 					return s;
 				}
-				case ClusterTypeGeneric:
-				{
+				case clusterTypeGeneric:{
 					var submitCommand = Environment.GetEnvironmentVariable("MQ_CLUSTER_SUBMIT_CMD");
 					var templatePath = Environment.GetEnvironmentVariable("MQ_CLUSTER_JOB_TEMPLATE_PATH");
-					if (templatePath != null)
-					{
+					if (templatePath != null){
 						return new GenericClusterSession(submitCommand, File.ReadAllText(templatePath));
 					}
-					
 					return new GenericClusterSession(submitCommand);
 				}
-				case ClusterTypeKubernetes:
-				{
+				case clusterTypeKubernetes:{
 					var ns = Environment.GetEnvironmentVariable("MQ_KUBERNETES_NAMESPACE") ?? "default";
 					var containerId = Environment.GetEnvironmentVariable("MQ_KUBERNETES_CONTAINER") ?? "mono";
 					// TODO: config
-					
 					var volumes = Environment.GetEnvironmentVariable("MQ_KUBERNETES_VOLUMES") ?? "";
 					var host = Environment.GetEnvironmentVariable("MQ_KUBERNETES_HOST");
 					return new KubernetesSession(ns, containerId, volumes, host);
@@ -96,56 +105,38 @@ namespace QueueingSystem {
 			}
 		}
 
-		public int GetTotalThreads()
-		{
+		public int GetTotalThreads(){
 			int res = 0;
-			for (int i = 0; i < NTasks; i++)
-			{
+			for (int i = 0; i < NTasks(); i++){
 				res += GetNumInternalThreads(i);
 			}
-
 			return res;
 		}
-		
-		public virtual int GetNumInternalThreads(int taskIndex)
-		{
+
+		public virtual int GetNumInternalThreads(int taskIndex){
 			return numInternalThreads;
 		}
-		
-		public CalculationType CalculationType { get; }
-		
-		public int MaxHeapSizeGb { get; set; } 
 
-		public int Nthreads { get; }
-	
-		public int NTasks { get; }
-		
-		public void Abort() {
-			if (workThreads != null) {
-				foreach (Thread t in workThreads.Where(t => t != null)) {
+		public void Abort(){
+			if (workThreads != null){
+				foreach (Thread t in workThreads.Where(t => t != null)){
 					t.Abort();
 				}
 			}
-			if (CalculationType == CalculationType.ExternalProcess && externalProcesses != null) {
-				foreach (Process process in externalProcesses) {
-					if (process != null && Util.IsRunning(process)) {
-						try {
+			if (CalculationType == CalculationType.ExternalProcess && externalProcesses != null){
+				foreach (Process process in externalProcesses){
+					if (process != null && Util.IsRunning(process)){
+						try{
 							process.Kill();
-						} catch (Exception) { }
+						} catch (Exception){ }
 					}
 				}
 			}
-
-			if (CalculationType == CalculationType.Queueing && queuedJobIds != null)
-			{
-				foreach (string jobId in queuedJobIds)
-				{
-					try
-					{
-						_session.JobControl(jobId, Action.Terminate);
-					}
-					catch (QueuingSystemException ex)
-					{
+			if (CalculationType == CalculationType.Queueing && queuedJobIds != null){
+				foreach (string jobId in queuedJobIds){
+					try{
+						session.JobControl(jobId, Action.Terminate);
+					} catch (QueuingSystemException ex){
 						// TODO: handle DrmaaExceptions
 						Console.Error.WriteLine(ex.ToString());
 					}
@@ -155,36 +146,34 @@ namespace QueueingSystem {
 			}
 		}
 
-		public void Start()
-		{			
+		public void Start(){
 			// TODO: remove in release, move Session.Init() to upper level  
 //			if (CalculationType == CalculationType.Queueing)
 //			{
 //				_session.Init();
 //			}
 			toBeProcessed = new Stack<int>();
-			for (int index = NTasks - 1; index >= 0; index--) {
+			for (int index = NTasksCached - 1; index >= 0; index--){
 				toBeProcessed.Push(index);
 			}
 			workThreads = new Thread[Nthreads];
 			externalProcesses = new Process[Nthreads];
 			queuedJobIds = new string[Nthreads];
-			
-			for (int i = 0; i < Nthreads; i++) {
-				workThreads[i] = new Thread(Work) {Name = "Thread " + i + " of " + GetType().Name};
+			for (int i = 0; i < Nthreads; i++){
+				workThreads[i] = new Thread(Work){Name = "Thread " + i + " of " + GetType().Name};
 				workThreads[i].Start(i);
 				Thread.Sleep(initialDelay);
 			}
-			while (true) {
+			while (true){
 				Thread.Sleep(1000);
 				bool busy = false;
-				for (int i = 0; i < Nthreads; i++) {
-					if (workThreads[i].IsAlive) {
+				for (int i = 0; i < Nthreads; i++){
+					if (workThreads[i].IsAlive){
 						busy = true;
 						break;
 					}
 				}
-				if (!busy) {
+				if (!busy){
 					break;
 				}
 			}
@@ -193,54 +182,50 @@ namespace QueueingSystem {
 //			{
 //				Session.Exit();	
 //			}
-			
+
 			// TODO: waiting for fs sync
 			// TODO: remove in release
 			string sleepTime = Environment.GetEnvironmentVariable("MQ_WORK_SLEEP");
-			if (sleepTime != null)
-			{
-				Thread.Sleep(int.Parse(sleepTime));	
+			if (sleepTime != null){
+				Thread.Sleep(int.Parse(sleepTime));
 			}
-			
-
-			
 		}
 
-		public string GetMessagePrefix() {
+		public string GetMessagePrefix(){
 			return MessagePrefix + " ";
 		}
 
 		public abstract void Calculation(string[] args, Responder responder);
 		public virtual bool IsFallbackPosition => true;
 
-		protected virtual string GetComment(int taskIndex) {
+		protected virtual string GetComment(int taskIndex){
 			return "";
 		}
 
-		protected abstract string Executable { get; }
-		protected abstract string ExecutableCore { get; }
+		protected abstract string Executable{ get; }
+		protected abstract string ExecutableCore{ get; }
 		protected abstract object[] GetArguments(int taskIndex);
-		protected abstract int Id { get; }
-		protected abstract string MessagePrefix { get; }
+		protected abstract int Id{ get; }
+		protected abstract string MessagePrefix{ get; }
 
-		private void Work(object threadIndex) {
-			while (toBeProcessed.Count > 0) {
+		private void Work(object threadIndex){
+			while (toBeProcessed.Count > 0){
 				int x;
-				lock (this) {
-					if (toBeProcessed.Count > 0) {
+				lock (this){
+					if (toBeProcessed.Count > 0){
 						x = toBeProcessed.Pop();
-					} else {
+					} else{
 						x = -1;
 					}
 				}
-				if (x >= 0) {
+				if (x >= 0){
 					DoWork(x, (int) threadIndex);
 				}
 			}
 		}
 
-		private void DoWork(int taskIndex, int threadIndex) {
-			switch (CalculationType) {
+		private void DoWork(int taskIndex, int threadIndex){
+			switch (CalculationType){
 				case CalculationType.ExternalProcess:
 					ProcessSingleRunExternalProcess(taskIndex, threadIndex);
 					break;
@@ -253,33 +238,28 @@ namespace QueueingSystem {
 			}
 		}
 
-		private IJobTemplate MakeJobTemplate(int taskIndex, int threadIndex, int numInternalThreads)
-		{
+		private IJobTemplate MakeJobTemplate(int taskIndex, int threadIndex, int numInternalThreads){
 			string cmd = GetCommandFilename().Trim('"');
-			
+
 			// TODO: refactor to a function?
 			List<string> args = new List<string>{"mono", "--optimize=all,float32", "--server", cmd};
 			args.AddRange(GetLogArgs(taskIndex, taskIndex));
 			args.Add(Id.ToString());
 			args.AddRange(GetStringArgs(taskIndex));
-
 			string jobName = $"{GetFilename()}_{taskIndex}_{threadIndex}";
-			
 			string randSuffix = Guid.NewGuid().ToString();
-			
+
 			// TODO: Separate folder for job stdout/stderr?
-			string outPath = Path.Combine(InfoFolder, $"{jobName}.{randSuffix}.out"); 
+			string outPath = Path.Combine(InfoFolder, $"{jobName}.{randSuffix}.out");
 			// TODO: Separate folder for job stdout/stderr?
-			string errPath = Path.Combine(InfoFolder, $"{jobName}.{randSuffix}.err"); 
-			
+			string errPath = Path.Combine(InfoFolder, $"{jobName}.{randSuffix}.err");
+
 			// Copying parent environment
 			Dictionary<string, string> env = new Dictionary<string, string>();
-			foreach (DictionaryEntry entry in Environment.GetEnvironmentVariables())
-			{
+			foreach (DictionaryEntry entry in Environment.GetEnvironmentVariables()){
 				env[entry.Key.ToString()] = entry.Value.ToString();
 			}
-
-			IJobTemplate jobTemplate = _session.AllocateJobTemplate();						
+			IJobTemplate jobTemplate = session.AllocateJobTemplate();
 			jobTemplate.Arguments = args.ToArray();
 			jobTemplate.OutputPath = outPath;
 			jobTemplate.ErrorPath = errPath;
@@ -289,15 +269,14 @@ namespace QueueingSystem {
 			jobTemplate.MaxMemorySize = MaxHeapSizeGb * 1024 * 1024 * 1024;
 			return jobTemplate;
 		}
-		
-		private void ProcessSingleRunQueueing(int taskIndex, int threadIndex, int numInternalThreads)
-		{
+
+		private void ProcessSingleRunQueueing(int taskIndex, int threadIndex, int numInternalThreads){
 			IJobTemplate jobTemplate = MakeJobTemplate(taskIndex, threadIndex, numInternalThreads);
 
 			// TODO: non atomic operation. When Aborted: job submmited, but queuedJobIds[threadIndex] not filled yet
-			string jobId = _session.Submit(jobTemplate);
+			string jobId = session.Submit(jobTemplate);
 			queuedJobIds[threadIndex] = jobId;
-			
+
 			// TODO: remove debug messages from future release
 			Console.WriteLine($@"Created jobTemplate:
   parent command line args: {string.Join(", ", Environment.GetCommandLineArgs())}
@@ -309,41 +288,32 @@ namespace QueueingSystem {
   memory:     {jobTemplate.MaxMemorySize}
 Submitted job {jobTemplate.JobName} with id: {jobId}
 ");
-
-			try
-			{
-				var status = _session.WaitForJobBlocking(jobId);
-				if (status != Status.Success)
-				{
-					Console.Error.WriteLine($"{jobTemplate.JobName}, jobId: {jobId}: \n"+jobTemplate.ReadStderr());
+			try{
+				var status = session.WaitForJobBlocking(jobId);
+				if (status != Status.Success){
+					Console.Error.WriteLine($"{jobTemplate.JobName}, jobId: {jobId}: \n" + jobTemplate.ReadStderr());
 					throw new Exception(
 						$"Exception during execution of external job: {jobTemplate.JobName}, jobId: {jobId}, status: {status}");
-				}
-				else
-				{
+				} else{
 					Console.WriteLine($"Job \"{jobTemplate.JobName}\" with id {jobId} finished successfully");
 				}
-			}
-			finally
-			{
+			} finally{
 				// TODO: Maybe introduce flag (cleanup or not, for debugging purposes)
 				jobTemplate.Cleanup();
 			}
-
-		
 		}
 
-		private void ProcessSingleRunExternalProcess(int taskIndex, int threadIndex) {
+		private void ProcessSingleRunExternalProcess(int taskIndex, int threadIndex){
 			bool isUnix = FileUtils.IsUnix();
 			string cmd = GetCommandFilename();
 			string args = GetLogArgsString(taskIndex, taskIndex) + GetCommandArguments(taskIndex);
 			ProcessStartInfo psi = Util.IsRunningOnMono() && !DotNetCore
-			                       // http://www.mono-project.com/docs/about-mono/releases/4.0.0/#floating-point-optimizations
+				// http://www.mono-project.com/docs/about-mono/releases/4.0.0/#floating-point-optimizations
 				? new ProcessStartInfo("mono", " --optimize=all,float32 --server " + cmd + " " + args)
 				: new ProcessStartInfo(cmd, args);
-			if (isUnix) {
+			if (isUnix){
 				psi.WorkingDirectory = Directory.GetDirectoryRoot(cmd);
-				if (MaxHeapSizeGb > 0) {
+				if (MaxHeapSizeGb > 0){
 					psi.EnvironmentVariables["MONO_GC_PARAMS"] = "max-heap-size=" + MaxHeapSizeGb + "g";
 				}
 			}
@@ -353,7 +323,7 @@ Submitted job {jobTemplate.JobName} with id: {jobId}
 			psi.UseShellExecute = false;
 			psi.RedirectStandardError = true;
 			psi.RedirectStandardOutput = true;
-			var externalProcess = new Process {StartInfo = psi};
+			var externalProcess = new Process{StartInfo = psi};
 			externalProcess.OutputDataReceived += (sender, eventArgs) => { Console.WriteLine(eventArgs.Data); };
 			externalProcess.ErrorDataReceived += (sender, eventArgs) => { Console.Error.WriteLine(eventArgs.Data); };
 			externalProcesses[threadIndex] = externalProcess;
@@ -362,43 +332,39 @@ Submitted job {jobTemplate.JobName} with id: {jobId}
 			externalProcesses[threadIndex].WaitForExit();
 			int exitcode = externalProcesses[threadIndex].ExitCode;
 			externalProcesses[threadIndex].Close();
-			if (exitcode != 0) {
+			if (exitcode != 0){
 				throw new Exception("Exception during execution of external process: " + processid);
 			}
 		}
 
-		private string GetName(int taskIndex) {
-			return GetFilename() + " (" + Util.IntString(taskIndex + 1, NTasks) + "/" + NTasks + ")";
+		private string GetName(int taskIndex){
+			return GetFilename() + " (" + Util.IntString(taskIndex + 1, NTasksCached) + "/" + NTasksCached + ")";
 		}
 
-		private string[] GetLogArgs(int taskIndex, int id)
-		{
-			
-			return new[]
-			{
+		private string[] GetLogArgs(int taskIndex, int id){
+			return new[]{
 				InfoFolder, GetFilename(), id.ToString(), GetName(taskIndex), GetComment(taskIndex), "Process",
 			};
 		}
-		
-		public string GetLogArgsString(int taskIndex, int id)
-		{
-			return string.Join(" ", GetLogArgs(taskIndex, id).Select(x => $"\"{x}\""))+" ";
+
+		public string GetLogArgsString(int taskIndex, int id){
+			return string.Join(" ", GetLogArgs(taskIndex, id).Select(x => $"\"{x}\"")) + " ";
 		}
 
-		public string GetFilename() {
+		public string GetFilename(){
 			return GetMessagePrefix().Trim().Replace("/", "").Replace("(", "_").Replace(")", "_").Replace(" ", "_");
 		}
 
-		public string GetCommandFilename() {
-			return "\"" + FileUtils.executablePath + Path.DirectorySeparatorChar + (DotNetCore ? ExecutableCore : Executable) +
-			       "\"";
+		public string GetCommandFilename(){
+			return "\"" + FileUtils.executablePath + Path.DirectorySeparatorChar +
+			       (DotNetCore ? ExecutableCore : Executable) + "\"";
 		}
 
-		public string GetCommandArguments(int taskIndex) {
+		public string GetCommandArguments(int taskIndex){
 			object[] o = GetArguments(taskIndex);
 			string[] args = new string[o.Length + 1];
 			args[0] = $"\"{Id}\"";
-			for (int i = 0; i < o.Length; i++) {
+			for (int i = 0; i < o.Length; i++){
 				object o1 = o[i];
 				string s = Parser.ToString(o1);
 				args[i + 1] = "\"" + s + "\"";
@@ -406,10 +372,10 @@ Submitted job {jobTemplate.JobName} with id: {jobId}
 			return StringUtils.Concat(" ", args);
 		}
 
-		private string[] GetStringArgs(int taskIndex) {
+		private string[] GetStringArgs(int taskIndex){
 			object[] o = GetArguments(taskIndex);
 			string[] args = new string[o.Length];
-			for (int i = 0; i < o.Length; i++) {
+			for (int i = 0; i < o.Length; i++){
 				args[i] = $"{o[i]}";
 			}
 			return args;
