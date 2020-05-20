2afd505295ed70b483c6066c80f6e99aca555cd0
Juergen Cox <cox@biochem.mpg.de>
2020-05-20 16:15:50 +0200

    typo

--- BaseLibS/Ms/MsInstrument.cs
+++ BaseLibS/Ms/MsInstrument.cs
@@ -36,7 +36,7 @@ namespace BaseLibS.Ms{
 		public double CentroidHalfWidthDefault => 35;
 		public bool CentroidHalfWidthInPpmDefault => true;
 		public bool CentroidMatchTolInPpmDefault => true;
-		public bool DiaAdaptiveMassAccuraryDefault => true;
+		public bool DiaAdaptiveMassAccuracyDefault => true;
 		public double DiaMassWindowFactorDefault => 3.3;
 		public double DiaCorrThresholdFeatureClusteringDefault => 0.85;
 		public double DiaInitialPrecMassTolPpmDefault => 20;
