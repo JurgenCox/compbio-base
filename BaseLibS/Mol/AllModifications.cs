af9be5eea742afa0a82875687a15773e50f3b277
Juergen Cox <cox@biochem.mpg.de>
2020-05-30 20:12:04 +0200

    read/write for AllModifications

--- BaseLibS/Mol/AllModifications.cs
+++ BaseLibS/Mol/AllModifications.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using BaseLibS.Util;
 
 namespace BaseLibS.Mol{
 	[Serializable]
@@ -36,6 +38,28 @@ namespace BaseLibS.Mol{
 			Modifications = new Dictionary<char, ushort[]>();
 			foreach (KeyValuePair<char, List<ushort>> p in inte){
 				Modifications.Add(p.Key, p.Value.ToArray());
+			}
+		}
+
+		public AllModifications(BinaryReader reader){
+			NTermModifications = FileUtils.ReadUshortArray(reader);
+			CTermModifications = FileUtils.ReadUshortArray(reader);
+			int len = reader.ReadInt32();
+			Modifications = new Dictionary<char, ushort[]>();
+			for (int i = 0; i < len; i++){
+				char key = reader.ReadChar();
+				ushort[] value = FileUtils.ReadUshortArray(reader);
+				Modifications.Add(key, value);
+			}
+		}
+
+		public void Write(BinaryWriter writer){
+			FileUtils.Write(NTermModifications, writer);
+			FileUtils.Write(CTermModifications, writer);
+			writer.Write(Modifications.Count);
+			foreach (KeyValuePair<char, ushort[]> pair in Modifications){
+				writer.Write(pair.Key);
+				FileUtils.Write(pair.Value, writer);
 			}
 		}
 	}
