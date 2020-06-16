347fcdb9eaebd160d194f2bf10fc5e0a86f6247a
cox <cox@PCJC05>
2020-06-16 23:27:31 +0200

    DiaRtPredictionSecondRoundDefault ) true

--- BaseLibS/Ms/MsInstrument.cs
+++ BaseLibS/Ms/MsInstrument.cs
@@ -59,7 +59,7 @@ namespace BaseLibS.Ms{
 		public double DiaMinPrecursorScoreDefault => 0.5;
 		public double DiaMinProfileCorrelationDefault => 0;
 		public bool DiaRtPredictionDefault => false;
-		public bool DiaRtPredictionSecondRoundDefault => false;
+		public bool DiaRtPredictionSecondRoundDefault => true;
 		public bool DiaNoMlDefault => false;
 		public double DiaXgBoostBaseScoreDefault => 0.4;
 		public double DiaXgBoostSubSampleDefault => 0.9;
