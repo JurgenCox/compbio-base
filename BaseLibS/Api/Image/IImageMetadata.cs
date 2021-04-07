4e2e415c21b8ee7b22a445fa966de5c67d5d1d96
bfrohn@biochem.mpg.de <bfrohn@biochem.mpg.de>
2021-04-07 10:04:21 +0200

    fMRI metadata events

--- BaseLibS/Api/Image/IImageMetadata.cs
+++ BaseLibS/Api/Image/IImageMetadata.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace BaseLibS.Api.Image {
-    public interface IImageMetadata : ICloneable{
+    public interface IImageMetadata : ICloneable {
         double[,,] RigidBodyTransformation { get; set; }
+        string[,] Events { get; set; }
         string GetBIDSEntity(string entityName);
     }
 }
