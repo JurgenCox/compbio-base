7ad67a7c29b413d981fd585c3371aea2a08ddf75
Juergen Cox <jurgen.cox@hotmail.com>
2022-01-04 16:57:58 +0100

    BoolParamWf

--- BaseLibS/Graph/CheckBoxModel.cs
+++ BaseLibS/Graph/CheckBoxModel.cs
@@ -1,16 +1,24 @@
 ﻿using System;
 using BaseLibS.Drawing;
-using BaseLibS.Graph.Scroll;
-namespace BaseLibS.Graph {
-	public class CheckBoxModel : ISimpleScrollableControlModel {
-		public event EventHandler Close;
-		public void ProcessCmdKey(Keys2 keyData){
+using BaseLibS.Graph.Base;
+namespace BaseLibS.Graph{
+	public class CheckBoxModel : BasicControlModel {
+		private readonly Pen2 boxPen = new Pen2(Color2.FromArgb(112, 112, 112));
+		private readonly Pen2 checkPen = new Pen2(Color2.FromArgb(72, 72, 72));
+		public bool Checked{ get; set; }
+		public event EventHandler CheckedChanged;
+
+		public override void OnPaint(IGraphics g, int width, int height) {
+			g.DrawRectangle(boxPen, 3, 3, 11, 11);
+			if (Checked){
+				g.DrawLine(checkPen, 4, 9, 7, 12);
+				g.DrawLine(checkPen, 7, 12, 14, 6);
+			}
 		}
-		public void InvalidateBackgroundImages(){
-		}
-		public void OnSizeChanged(){
-		}
-		public void Register(ISimpleScrollableControl control){
+		public override void OnMouseClick(BasicMouseEventArgs e){
+			Checked = !Checked;
+			CheckedChanged?.Invoke(this, EventArgs.Empty);
+			Invalidate();
 		}
 	}
 }
--- BaseLibS/Param/BoolParamWf.cs
+++ BaseLibS/Param/BoolParamWf.cs
@@ -1,32 +1,31 @@
 using System;
-using System.Windows.Forms;
-using BaseLibS.Param;
-namespace BaseLib.Param{
+using BaseLibS.Graph;
+namespace BaseLibS.Param{
 	[Serializable]
-	internal class BoolParamWf : BoolParam{
-		[NonSerialized] private CheckBox control;
-		internal BoolParamWf(string name) : base(name){
+	public class BoolParamWf : BoolParam{
+		[NonSerialized] private CheckBoxModel control;
+		public BoolParamWf(string name) : base(name){
 		}
-		internal BoolParamWf(string name, bool value) : base(name, value){
+		public BoolParamWf(string name, bool value) : base(name, value){
 		}
-		protected BoolParamWf(string name, string help, string url, bool visible, bool value, bool default1) : base(
+		public BoolParamWf(string name, string help, string url, bool visible, bool value, bool default1) : base(
 			name, help, url, visible, value, default1){
 		}
 		public override ParamType Type => ParamType.WinForms;
 		public override void SetValueFromControl(){
-			if (control == null || control.IsDisposed){
+			if (control == null){
 				return;
 			}
 			Value = control.Checked;
 		}
 		public override void UpdateControlFromValue(){
-			if (control == null || control.IsDisposed){
+			if (control == null){
 				return;
 			}
 			control.Checked = Value;
 		}
 		public override object CreateControl(){
-			control = new CheckBox{Checked = Value};
+			control = new CheckBoxModel{Checked = Value};
 			control.CheckedChanged += (sender, e) => { SetValueFromControl(); };
 			return control;
 		}
