60c1b31f56657c4791a4173520af9934b7b31d6a
Juergen Cox <jurgen.cox@hotmail.com>
2023-01-11 09:13:10 +0100

    Enzyme.CleavesN/Cterminally() added

--- BaseLibS/Mol/Enzyme.cs
+++ BaseLibS/Mol/Enzyme.cs
@@ -34,33 +34,65 @@ namespace BaseLibS.Mol{
 			return false;
 		}
 
-		public string GetSpecialAas(){
+		public string GetSpecialAas() {
 			Dictionary<char, int> count1 = new Dictionary<char, int>();
 			Dictionary<char, int> count2 = new Dictionary<char, int>();
-			foreach (string s in specificity){
+			foreach (string s in specificity) {
 				char c1 = s[0];
 				char c2 = s[1];
-				if (!count1.ContainsKey(c1)){
+				if (!count1.ContainsKey(c1)) {
 					count1.Add(c1, 0);
 				}
-				if (!count2.ContainsKey(c2)){
+				if (!count2.ContainsKey(c2)) {
 					count2.Add(c2, 0);
 				}
 				count1[c1]++;
 				count2[c2]++;
 			}
 			HashSet<char> result = new HashSet<char>();
-			foreach (char c in count1.Keys){
-				if (count1[c] >= 17){
+			foreach (char c in count1.Keys) {
+				if (count1[c] >= 17) {
 					result.Add(c);
 				}
 			}
-			foreach (char c in count2.Keys){
-				if (count2[c] >= 17){
+			foreach (char c in count2.Keys) {
+				if (count2[c] >= 17) {
 					result.Add(c);
 				}
 			}
 			return new string(result.ToArray());
+		}
+		public bool CleavesCterminally() {
+			Dictionary<char, int> count1 = new Dictionary<char, int>();
+			foreach (string s in specificity) {
+				char c1 = s[0];
+				if (!count1.ContainsKey(c1)) {
+					count1.Add(c1, 0);
+				}
+				count1[c1]++;
+			}
+			foreach (char c in count1.Keys) {
+				if (count1[c] >= 17) {
+					return true;
+				}
+			}
+			return false;
+		}
+		public bool CleavesNterminally() {
+			Dictionary<char, int> count2 = new Dictionary<char, int>();
+			foreach (string s in specificity) {
+				char c2 = s[1];
+				if (!count2.ContainsKey(c2)) {
+					count2.Add(c2, 0);
+				}
+				count2[c2]++;
+			}
+			foreach (char c in count2.Keys) {
+				if (count2[c] >= 17) {
+					return true;
+				}
+			}
+			return false;
 		}
 	}
 }
