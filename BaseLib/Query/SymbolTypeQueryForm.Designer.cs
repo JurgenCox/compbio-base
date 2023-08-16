0a27639679538b34e85ec772f622f58d9ce362c9
Jurgen <cox@biochem.mpg.de>
2023-08-16 09:22:05 +0200

    SymbolTypeQueryForm pdated

--- BaseLib/Query/SymbolTypeQueryForm.Designer.cs
+++ BaseLib/Query/SymbolTypeQueryForm.Designer.cs
@@ -113,6 +113,7 @@
 			// comboBox1
 			// 
 			this.comboBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
 			this.comboBox1.FormattingEnabled = true;
 			this.comboBox1.Location = new System.Drawing.Point(103, 3);
 			this.comboBox1.Name = "comboBox1";
