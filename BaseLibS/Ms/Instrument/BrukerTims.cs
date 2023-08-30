2f0d3afe0983d828e9ba2e01f5375eb2d9068555
Jurgen <cox@biochem.mpg.de>
2023-08-31 00:55:38 +0200

    IntensityThresholdMs1DiaDefault => 30

--- BaseLibS/Ms/Instrument/BrukerTims.cs
+++ BaseLibS/Ms/Instrument/BrukerTims.cs
@@ -5,8 +5,8 @@ namespace BaseLibS.Ms.Instrument{
 	public class BrukerTims : QtofInstrument{
 		public BrukerTims(int index) : base(index){ }
 		public override string Name => "Bruker TIMS";
-		public override double IntensityThresholdMs1DdaDefault => 70;
-		public override double IntensityThresholdMs1DiaDefault => 70;
+		public override double IntensityThresholdMs1DdaDefault => 30;
+		public override double IntensityThresholdMs1DiaDefault => 30;
 		public override double IntensityThresholdMs2Default => 30;
 		public override double DiaMinMsmsIntensityForQuantDefault => 30;
 		public sealed override bool UseMs1CentroidsDefault => false;
